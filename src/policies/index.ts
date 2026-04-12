eff4499b03010e2dc847a0c451883a56c5e5c10f
Eran Sandler <eran@sandler.co.il>
2026-04-12 16:33:15 -0700

    feat: re-export new v0.18.0 policy types from policies index

--- src/policies/index.ts
+++ src/policies/index.ts
@@ -1,5 +1,20 @@
-export { PolicyDefinitionSchema, validatePolicy } from './schema.js';
-export type { PolicyDefinition, FileRule, NetworkRule, CommandRule, EnvRule, DnsRedirect, ConnectRedirect } from './schema.js';
+export { PolicyDefinitionSchema, HttpServiceSchema, SecretProviderSchema, validatePolicy } from './schema.js';
+export type {
+  PolicyDefinition,
+  FileRule,
+  NetworkRule,
+  CommandRule,
+  EnvRule,
+  DnsRedirect,
+  ConnectRedirect,
+  SecretProvider,
+  VaultAuth,
+  HttpService,
+  HttpServiceRule,
+  HttpServiceSecret,
+  HttpServiceInject,
+  HttpServiceInjectHeader,
+} from './schema.js';
 export { agentDefault, devSafe, ciStrict, agentSandbox } from './presets.js';
 export { merge, mergePrepend } from './merge.js';
 export { serializePolicy, systemPolicyYaml } from './serialize.js';
